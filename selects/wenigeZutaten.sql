dd78942f7e12ae0f7d8e2725363444deb71bdff9
schimminsky <jean-luc@leminsky.net>
2025-11-07 10:11:27 +0100

    select statement for recipes with number of ingredients below 6

---
+++ selects/wenigeZutaten.sql
@@ -0,0 +1,9 @@
+-- Rezepte mit wenigen Zutaten finden: 
+-- Wählt alle Rezepte aus, die weniger als fünf Zutaten enthalten.
+
+SELECT r.NAME ,
+COUNT(DISTINCT rz.ZUTATENID) AS 'Anzahl Zutaten'
+FROM REZEPT r
+JOIN REZEPT_ZUTAT rz ON r.REZEPTID = rz.REZEPTID
+GROUP BY r.NAME
+HAVING 'Anzahl Zutaten' < 6;
