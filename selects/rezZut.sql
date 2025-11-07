8986f47b3d8cd500eb0c0acf45f22d97f3519306
schimminsky <jean-luc@leminsky.net>
2025-11-07 11:11:47 +0100

    add optional procedure

--- selects/rezZut.sql
+++ selects/rezZut.sql
@@ -6,3 +6,18 @@ JOIN REZEPT_ZUTAT rz ON rez.REZEPTID = rz.REZEPTID
 JOIN ZUTAT zut ON zut.ZUTATENID = rz.ZUTATENID
 WHERE zut.NAME IN ('')
 ;
+
+
+-- TODO: lookup how procedures work, not important
+-- DELIMITER //
+
+-- CREATE PROCEDURE IF NOT EXISTS procedure_filter_ingredient (IN zutat VARCHAR(1000), OUT rezepte VARCHAR(1000))
+-- BEGIN 
+-- SELECT rez.NAME AS 'Rezeptname'
+-- FROM REZEPT rez
+-- JOIN REZEPT_ZUTAT rz ON rez.REZEPTID = rz.REZEPTID
+-- JOIN ZUTAT zut ON zut.ZUTATENID = rz.ZUTATENID
+-- WHERE zut.NAME IN (zutat)
+-- ;
+-- END;
+-- // DELIMITER;
